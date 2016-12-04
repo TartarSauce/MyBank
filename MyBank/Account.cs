a50dcb3a498fc51003899144e3f54d1729284f43
TartarSauce <uma_bangalore@hotmail.com>
2016-12-04 09:50:45 -0800

    Initial commit

---
+++ MyBank/Account.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    public enum typeOfAccount { Checking, Saving};
+    /// <summary>
+    /// This class describes a bank account
+    /// </summary>
+    class Account
+    {
+        #region Statics
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        public int AccountNumber {
+            get;
+            private set;
+        }
+        public string EmailAddress { get; set; }
+        public decimal Balance { get; private set; }
+        public typeOfAccount AccountType { get; set; }
+        #endregion
+
+        #region Contructors
+        public Account()
+        {
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
+        #region Methods
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO MY BANK");
+            var choice = "-1";
+            while (choice != "0")
+            {
+                Console.WriteLine("*******************************************");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                Console.WriteLine("*******************************************");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "0":
+                        Console.WriteLine("have a great day!");
+                        return;
+                    case "1":
+                        Console.Write("Email address: ");
+                        var checkingAccount = new Account();
+                        checkingAccount.EmailAddress = Console.ReadLine();
+                        checkingAccount.AccountType = typeOfAccount.Checking;
+                        checkingAccount.Deposit(1002.50M);
+                        Console.WriteLine($"Account Number: {checkingAccount.AccountNumber}, Balance: {checkingAccount.Balance:C}");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
